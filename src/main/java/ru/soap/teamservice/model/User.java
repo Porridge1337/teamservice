d12f72bdb5f50e2ebbeac6234fc6c03a533641b9
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-10-02 22:39:03 +0300

    Добавил новые поля для юзеров

--- src/main/java/ru/soap/teamservice/model/User.java
+++ src/main/java/ru/soap/teamservice/model/User.java
@@ -10,9 +10,11 @@ import lombok.NoArgsConstructor;
 public class User {
 
     private int id;
+    private String login;
     private String username;
     private String surname;
-    private String telegramLogin;
+    private String telegramUser;
+    private long telegramId;
     private String phone;
     private Role role;
     private Group group;
