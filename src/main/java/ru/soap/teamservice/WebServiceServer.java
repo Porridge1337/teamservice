d06822285cbcbc65d47f735e3d158cd96f659fed
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-10-02 22:44:11 +0300

    Исправление параметра в  @WebParam для DaoRole (name = "new-save-role")

--- src/main/java/ru/soap/teamservice/WebServiceServer.java
+++ src/main/java/ru/soap/teamservice/WebServiceServer.java
@@ -1,5 +1,7 @@
 package ru.soap.teamservice;
 
+import ru.soap.teamservice.model.Group;
+import ru.soap.teamservice.model.Role;
 import ru.soap.teamservice.model.User;
 import ru.soap.teamservice.service.DaoRole;
 import ru.soap.teamservice.service.DaoUser;
@@ -13,14 +15,15 @@ import java.util.List;
 public class WebServiceServer {
 
     public static void main(String[] args) {
-        String bindingURI1 = "http://localhost:9898/team1";
+       /* String bindingURI1 = "http://localhost:9898/team1";
         String bindingURI2 = "http://localhost:9898/team2";
-        DaoUser daoUser = new DaoUserImpl();
-        DaoRole daoRole = new DaoRoleImpl();
+
         Endpoint.publish(bindingURI1, daoUser);
         Endpoint.publish(bindingURI2, daoRole);
         System.out.println("Server started at: " + bindingURI1);
-        System.out.println("Server started at: " + bindingURI2);
+        System.out.println("Server started at: " + bindingURI2);*/
+        DaoUser daoUser = new DaoUserImpl();
+        DaoRole daoRole = new DaoRoleImpl();
 
 
 
