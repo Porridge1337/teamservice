25aa26655e71f2e3c91d621ec90f8f0a6518c57c
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-10-03 02:25:32 +0300

    Реализация абстракции DAO group

--- src/main/java/ru/soap/teamservice/service/DaoGroup.java
+++ src/main/java/ru/soap/teamservice/service/DaoGroup.java
@@ -1,4 +1,30 @@
 package ru.soap.teamservice.service;
 
+import ru.soap.teamservice.model.Group;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.xml.ws.RequestWrapper;
+import java.util.List;
+
+@WebService
 public interface DaoGroup {
+
+    @WebMethod(operationName = "findAllGroups")
+    @RequestWrapper(className = "ru.soap.teamservice.service.jaxws.FindAllGroups", localName = "findAllGroups")
+    List<Group> findAllGroups();
+
+    @WebMethod(operationName = "saveGroup")
+    @RequestWrapper(className = "ru.soap.teamservice.service.jaxws.SaveGroup", localName = "saveGroup")
+    boolean saveGroup(@WebParam(name = "saved-group") Group group);
+
+    @WebMethod(operationName = "updateGroupById")
+    @RequestWrapper(className = "ru.soap.teamservice.service.jaxws.UpdateGroupById", localName = "updateGroupById")
+    boolean updateGroup(@WebParam(name = "updated-group") Group group);
+
+    @WebMethod(operationName = "deleteGroupById")
+    @RequestWrapper(className = "ru.soap.teamservice.service.jaxws.DeleteGroupById", localName = "deleteGroupById")
+    boolean deleteGroupById(@WebParam(name = "group id to be deleted") int id);
+
 }
