ef436d9567aa80ce11453077df74a6019bb7e966
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-10-02 04:59:11 +0300

    Тут эндпоинты и запуск серверной части веб-сервиса

---
+++ src/main/java/ru/soap/teamservice/WebServiceServer.java
@@ -0,0 +1,29 @@
+package ru.soap.teamservice;
+
+import ru.soap.teamservice.model.User;
+import ru.soap.teamservice.service.DaoRole;
+import ru.soap.teamservice.service.DaoUser;
+import ru.soap.teamservice.service.impl.DaoRoleImpl;
+import ru.soap.teamservice.service.impl.DaoUserImpl;
+
+import javax.xml.ws.Endpoint;
+import java.util.List;
+
+
+public class WebServiceServer {
+
+    public static void main(String[] args) {
+        String bindingURI1 = "http://localhost:9898/team1";
+        String bindingURI2 = "http://localhost:9898/team2";
+        DaoUser daoUser = new DaoUserImpl();
+        DaoRole daoRole = new DaoRoleImpl();
+        Endpoint.publish(bindingURI1, daoUser);
+        Endpoint.publish(bindingURI2, daoRole);
+        System.out.println("Server started at: " + bindingURI1);
+        System.out.println("Server started at: " + bindingURI2);
+
+
+
+    }
+
+}
