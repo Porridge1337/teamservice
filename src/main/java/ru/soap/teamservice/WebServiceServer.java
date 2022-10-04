06fba8b35ee5c6a581678eb4374e135050138e18
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-10-04 21:37:55 +0300

    Инициализация конфига для логгера

--- src/main/java/ru/soap/teamservice/WebServiceServer.java
+++ src/main/java/ru/soap/teamservice/WebServiceServer.java
@@ -1,5 +1,7 @@
 package ru.soap.teamservice;
 
+
+import ru.soap.teamservice.config.LoggerConfig;
 import ru.soap.teamservice.service.DaoGroup;
 import ru.soap.teamservice.service.DaoRole;
 import ru.soap.teamservice.service.DaoUser;
@@ -8,18 +10,20 @@ import ru.soap.teamservice.service.impl.DaoRoleImpl;
 import ru.soap.teamservice.service.impl.DaoUserImpl;
 
 import javax.xml.ws.Endpoint;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 
 public class WebServiceServer {
     private static Logger logger = Logger.getLogger(WebServiceServer.class.getName());
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        LoggerConfig.initialize();
         String useruri = "http://localhost:9898/Users";
         String roleuri = "http://localhost:9898/Roles";
         String groupuri = "http://localhost:9898/Groups";
 
-        logger.info("This is level info logging");
         DaoUser daoUser = new DaoUserImpl();
         DaoRole daoRole = new DaoRoleImpl();
         DaoGroup daoGroup = new DaoGroupImpl();
@@ -30,9 +34,7 @@ public class WebServiceServer {
 
         logger.info("Server started at: " + useruri + "?wsdl");
         logger.info("Server started at: " + roleuri + "?wsdl");
-        logger.info("Server started at: " + groupuri + "?wsdl") ;
-
-
+        logger.info("Server started at: " + groupuri + "?wsdl");
     }
 
 }
