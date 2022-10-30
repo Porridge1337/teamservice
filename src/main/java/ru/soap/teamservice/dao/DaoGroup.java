a93ee071451d6d1193338b2244508f1b97ab0030
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-10-30 17:03:20 +0300

    Убрал ненужные аннотации

--- src/main/java/ru/soap/teamservice/dao/DaoGroup.java
+++ src/main/java/ru/soap/teamservice/dao/DaoGroup.java
@@ -2,16 +2,15 @@ package ru.soap.teamservice.dao;
 
 import ru.soap.teamservice.model.Group;
 
-import javax.jws.WebParam;
 import java.util.List;
 
 public interface DaoGroup {
 
     List<Group> findAllGroups();
 
-    boolean saveGroup(@WebParam(name = "saved-group") Group group);
+    boolean saveGroup(Group group);
 
-    boolean updateGroup(@WebParam(name = "updated-group") Group group);
+    boolean updateGroup(Group group);
 
     boolean deleteGroupById(int id);
 
